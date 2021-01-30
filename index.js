284cd754cc21380b94b4aaf23565800b56958604
acorpuz <adriancorpuz@gmail.com>
2021-01-29 19:36:04 -0600

    Added static for express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
--- public/main.js
+++ public/main.js
@@ -6,14 +6,14 @@ let userName = "@";
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 	if (input.value) {
-	  socket.emit('chat message', input.value);
+	  socket.emit('chat message', userName.padEnd("15", " ") + input.value);
 	  input.value = '';
 	}
 });
 
 socket.on('chat message', function(msg) {
 var item = document.createElement('li');
-	item.textContent = userName + ": " + msg;
+	item.textContent = msg;
 	messages.appendChild(item);
 	window.scrollTo(0, document.body.scrollHeight);
 });
@@ -23,6 +23,7 @@ var item = document.createElement('li');
 const newUserConnected = (user) => {
   userName = user || `User${Math.floor(Math.random() * 1000000)}`;
   socket.emit("new user", userName);
+  socket.emit("chat message", userName + " connected to the chat.");
   addToUsersBox(userName);
 };
 
